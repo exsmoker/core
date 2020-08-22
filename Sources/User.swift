6efa4acb15cb633dd51fbe69f06b5dd278845419
vauxhall <zero.griffin@gmail.com>
2020-08-22 15:36:17 +0200

    Add init

--- Sources/User.swift
+++ Sources/User.swift
@@ -7,6 +7,8 @@ public struct User: Codable, Equatable {
     public var rated = false
     public var created = Date()
     
+    public init() { }
+    
     public func hash(into: inout Hasher) { }
     
     public static func == (lhs: Self, rhs: Self) -> Bool {
