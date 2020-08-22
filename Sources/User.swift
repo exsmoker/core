41c01ffea49e17e09e7f60a7d9ddf28bc834e3c2
vauxhall <zero.griffin@gmail.com>
2020-08-22 15:28:53 +0200

    Create user

---
+++ Sources/User.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public struct User: Codable, Equatable {
+    public var name = ""
+    public var location = ""
+    public var premium = false
+    public var rated = false
+    public var created = Date()
+    
+    public func hash(into: inout Hasher) { }
+    
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        true
+    }
+}
