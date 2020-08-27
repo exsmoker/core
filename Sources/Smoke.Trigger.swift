f61242e86d9c5459ea340ea4e698a278169fac5b
vauxhall <zero.griffin@gmail.com>
2020-08-27 09:09:09 +0200

    Update trigger

--- Sources/Smoke.Trigger.swift
+++ Sources/Smoke.Trigger.swift
@@ -1,7 +1,20 @@
 import Foundation
 
 extension Smoke {
-    public enum Trigger: String, Codable {
+    public enum Trigger: String, Codable, CaseIterable {
+        case
+        somebodySmoking,
+        bored,
+        anxious,
+        nervous,
+        feelLike,
+        doNotKnow,
+        noReason,
+        cold,
+        tired
         
+        var title: String {
+            "Trigger.title.\(self)"
+        }
     }
 }
