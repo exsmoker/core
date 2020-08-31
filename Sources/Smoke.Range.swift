e0195e6194bb053d4318675b8106c83be973b95e
vauxhall <zero.griffin@gmail.com>
2020-08-31 12:49:21 +0200

    Update components

--- Sources/Smoke.Range.swift
+++ Sources/Smoke.Range.swift
@@ -12,17 +12,29 @@ extension Smoke {
         }
         
         func filter(_ hits: [Hit]) -> [Double] {
-            [0, 0, 0, 0, 0]
+            var items = self.items
+            hits.map {
+                components($0.date)
+            }.forEach {
+                items[$0]? += 1
+            }
+            
+            items.values.max()
+            items.map(<#T##transform: ((key: DateComponents, value: Int)) throws -> T##((key: DateComponents, value: Int)) throws -> T#>)
+            
+            return [0, 0, 0, 0, 0]
         }
         
         private func components(_ date: Date) -> DateComponents {
             Calendar.current.dateComponents(components, from: date)
         }
         
-        private var items: [DateComponents] {
+        private var items: [DateComponents : Int] {
             (0 ... 4).reversed().map {
                 Calendar.current.date(byAdding: component, value: -$0, to: .init())!
-            }.map(components)
+            }.map(components).reduce(into: [:]) {
+                $0[$1] = 0
+            }
         }
         
         private var components: Set<Calendar.Component> {
